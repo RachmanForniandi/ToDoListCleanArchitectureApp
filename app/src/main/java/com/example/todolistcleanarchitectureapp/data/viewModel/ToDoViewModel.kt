e5b749111755c86ef4bf5f2727be840107b6b2bd
RachmanForniandi <rachmanforniandi@gmail.com>
2020-12-21 07:35:43 +0700

    create viewModel class done.

---
+++ app/src/main/java/com/example/todolistcleanarchitectureapp/data/viewModel/ToDoViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.todolistcleanarchitectureapp.data.viewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.todolistcleanarchitectureapp.data.ToDoData
+import com.example.todolistcleanarchitectureapp.data.ToDoDatabase
+import com.example.todolistcleanarchitectureapp.data.repository.ToDoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ToDoViewModel(application: Application):AndroidViewModel(application) {
+
+    private val toDoDao = ToDoDatabase.getDatabase(application).todoDao()
+
+    private val repository: ToDoRepository
+
+    private val getAllData:LiveData<List<ToDoData>>
+
+    init {
+        repository = ToDoRepository(toDoDao)
+        getAllData = repository.fetchAllData
+    }
+
+    fun insertData(toDoData: ToDoData){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.insertData(toDoData)
+        }
+    }
+
+    
+
+}
