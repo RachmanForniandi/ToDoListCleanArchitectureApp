5a947fc5b53d327901bd36094f6a6d935c38be2d
RachmanForniandi <rachmanforniandi@gmail.com>
2020-12-30 08:44:05 +0700

    databinding for list fragment done.

---
+++ app/src/main/java/com/example/todolistcleanarchitectureapp/fragments/BindingAdapters.kt
@@ -0,0 +1,34 @@
+package com.example.todolistcleanarchitectureapp.fragments
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.MutableLiveData
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.todolistcleanarchitectureapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class BindingAdapters {
+
+    companion object{
+        @BindingAdapter("android:navigateToAddFragment")
+        @JvmStatic
+        fun navigateToAddFragment(view:FloatingActionButton, navigate: Boolean){
+            view.setOnClickListener {
+                view.findNavController().navigate(R.id.action_listFragment_to_addFragment)
+            }
+        }
+
+        @BindingAdapter("android:emptyDatabase")
+        @JvmStatic
+        fun emptyDatabase(view: View, emptyDatabase: MutableLiveData<Boolean>){
+            when(emptyDatabase.value){
+                true-> view.visibility = View.VISIBLE
+                false-> view.visibility = View.INVISIBLE
+            }
+        }
+
+
+    }
+
+}
